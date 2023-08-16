738e5960d439527b72f1f1a22de0ffe30667ac80
chandiosamad07 <absamadchandio@gmail.com>
2023-08-16 15:34:05 +0500

    Components

---
+++ myapp/src/components/Greet.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// function Greet() {
+//   return (
+//     <div>
+//       <h1>Hello samad</h1>
+//     </div>
+//   )
+// }
+
+const Greet = ()=> {
+    return(
+        <h1>samad</h1>
+    )
+    
+}
+export default Greet
