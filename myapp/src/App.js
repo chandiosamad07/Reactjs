14506601e6e49536ff1e3b27ae98160b0d41e069
chandiosamad07 <absamadchandio@gmail.com>
2023-08-16 16:20:11 +0500

    State

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,17 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Greet from "./components/Greet";
-import Welcome from "./components/Welcome";
+//import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
+import Message from "./components/Message";
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Abdul"/>
+      <Message/>
+      
+      {/* <Greet name="Abdul"/>
       <Greet name="Samad"/>
       <Greet name="Chandio"/>
       <Welcome />
-      <Hello></Hello>
+      <Hello></Hello> */}
     </div>
   );
 }
