f108d7fcc413afa4bbf6661c338734908c5f07dd
chandiosamad07 <absamadchandio@gmail.com>
2023-08-20 13:32:51 +0500

    Conditional Rendering variable method

--- myapp/src/components/UserGreeting.js
+++ myapp/src/components/UserGreeting.js
@@ -10,11 +10,21 @@ class UserGreeting extends Component {
   }
 
   render() {
-    if (this.state.isloggined) {
-      return <div>Welcome Samad</div>;
-    } else {
-      return <div>Welcome Guest</div>;
+
+    let message
+    if(this.state.isloggined){
+        message = <div>Welcome Abdul Samad</div>
     }
+    else{
+        message = <div>Welcome Guest</div>
+    }
+
+    return <div>{message}</div>
+    // if (this.state.isloggined) {
+    //   return <div>Welcome Samad</div>;
+    // } else {
+    //   return <div>Welcome Guest</div>;
+    // }
   }
 }
 
