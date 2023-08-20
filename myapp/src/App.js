41ccfe984e93425b799e2d277f3b90b4f420c398
chandiosamad07 <absamadchandio@gmail.com>
2023-08-20 14:24:02 +0500

    Inline Css

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -13,10 +13,12 @@ import ParentComponent from "./components/ParentComponent";
 import UserGreeting from "./components/UserGreeting";
 import NameList from "./components/NameList";
 import Stylesheet from "./components/Stylesheet";
+import Inline from "./components/Inline";
 function App() {
   return (
     <div className="App">
-     <Stylesheet></Stylesheet>
+      <Inline />
+     {/* <Stylesheet></Stylesheet> */}
       {/* <NameList/> */}
       {/* <UserGreeting /> */}
       {/* <ParentComponent /> */}
--- myapp/src/components/Stylesheet.js
+++ myapp/src/components/Stylesheet.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './myStyle.css'
 function Stylesheet() {
+    // let className = props.primary ? 'primary': ''
   return (
     <div>
       <h1 className='primary'>This is Stylesheet</h1>
