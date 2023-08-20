efdd67c50bfc0fa8a5cbd2d6166ddf4fbcfca4a5
chandiosamad07 <absamadchandio@gmail.com>
2023-08-20 14:04:31 +0500

    Styling and css

---
+++ myapp/src/components/Stylesheet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './myStyle.css'
+function Stylesheet() {
+  return (
+    <div>
+      <h1 className='primary'>This is Stylesheet</h1>
+    </div>
+  )
+}
+
+export default Stylesheet
