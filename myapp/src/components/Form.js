545d0f9652bda37f89845a5e179ef1a5098582b8
chandiosamad07 <absamadchandio@gmail.com>
2023-08-22 14:27:05 +0500

    Forms

--- myapp/src/components/Form.js
+++ myapp/src/components/Form.js
@@ -33,23 +33,26 @@ import React, { Component } from 'react'
     }
 
   render() {
+    const [username , comment, topic] = this.state
     return (
       <form onSubmit={this.handleSubmit}>
         <div>
         <label>User Name</label>
-        <input type='text' value={this.state.username} onChange={this.handlerNameChange}></input>
+        <input type='text' value={username} onChange={this.handlerNameChange}></input>
         </div>
         <div>
             <label>Comments</label>
-            <textarea value={this.state.comment} onChange={this.handlerCommentChange}></textarea>
+            <textarea value={comment} onChange={this.handlerCommentChange}></textarea>
         </div>
 
         <div>
             <label>Topic</label>
-           <select value={this.state.topic} onChange={this.state.handlerTopicChange}>
+           <select value={topic} onChange={this.state.handlerTopicChange}>
             <option value="react"> React</option>
             <option value="Angular"> Angular</option>
             <option value="Vue"> Vue</option>
+            <option value="Vue"> js</option>
+            
            </select>
         </div>
         
