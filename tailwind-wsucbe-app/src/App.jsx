78bdc9f43f41d16811a0f3688ad220dae837592f
chandiosamad07 <absamadchandio@gmail.com>
2023-12-13 14:32:49 +0500

    Header and Hero with responsive

---
+++ tailwind-wsucbe-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Components/Header";
+import Banner from "./Components/Banner";
+function App() {
+  return (
+    <>
+    <Header />
+    <Banner/>
+  {/* //   <div className="bg-blue-500 text-white p-4">
+  //   This is a component styled with Tailwind CSS!
+  // </div> */}
+  </>
+  );
+}
+
+export default App;
