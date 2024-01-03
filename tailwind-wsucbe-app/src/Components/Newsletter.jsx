1408aad023590a4f4ea3cc0dc48e9653eeb76470
chandiosamad07 <absamadchandio@gmail.com>
2024-01-03 12:30:44 +0500

    Newsletter

--- tailwind-wsucbe-app/src/Components/Newsletter.jsx
+++ tailwind-wsucbe-app/src/Components/Newsletter.jsx
@@ -3,8 +3,15 @@ import React from 'react'
 const Newsletter = () => {
   return (
     <div className='bg-[#2699fb] p-4'>
-     <div className='max-w-[1240px] flex'></div>
-     
+     <div className='max-w-[1240px] mx-auto flex justify-between py-[50px]'>
+      <div className='border border-black h-[300px]'>
+        <h1 className='text[40px] font-bold  text-white'>Want to learn latest I.T skills?</h1>
+      <span className='text-white'>
+        Sign up to our Newsletter and stay up to date.
+      </span>
+      </div>
+      <div className='border border-black h-[300px]'></div>
+     </div>
     </div>
   )
 }
