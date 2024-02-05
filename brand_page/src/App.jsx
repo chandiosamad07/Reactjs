3c025da1156a0ac348106c61196c640dd5eb8699
chandiosamad07 <absamadchandio@gmail.com>
2024-02-05 12:01:51 +0500

    brand page Hero section content

--- brand_page/src/App.jsx
+++ brand_page/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import Header from './Components/Header'
 import './App.css'
+import Hero from './Components/Hero'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
     <Header />
+    <Hero />
     </>
   )
 }
