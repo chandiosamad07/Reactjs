640cdb8f36f54a8766b2814f9640d6bcb0f4c067
chandiosamad07 <absamadchandio@gmail.com>
2024-02-05 23:09:43 +0500

    React Gallery using profile

--- onlinelearning/src/App.js
+++ onlinelearning/src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Component from './components/component';
 import Profile from './components/Profile';
+import Gallery from './components/Gallery';
+
 function App() {
   return (
   <>
-  <Component />
-  <Profile />
+  {/* <Component />
+  <Profile /> */}
+  <Gallery />
   </>
   );
 }
