146229cf1cd77e861faf82354666aec9e2c7c784
chandiosamad07 <absamadchandio@gmail.com>
2024-02-05 23:04:08 +0500

    React Profile using Image

--- onlinelearning/src/App.js
+++ onlinelearning/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Component from './components/component';
+import Profile from './components/Profile';
 function App() {
   return (
   <>
   <Component />
+  <Profile />
   </>
   );
 }
