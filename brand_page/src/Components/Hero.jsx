dc63b1582077a3345a10ffe9240ba29b92b3d4b3
chandiosamad07 <absamadchandio@gmail.com>
2024-02-05 12:21:00 +0500

    Brand page hero section content style buttons

--- brand_page/src/Components/Hero.jsx
+++ brand_page/src/Components/Hero.jsx
@@ -4,21 +4,24 @@ const Hero = () => {
   return (
     <main className="hero">
       <div className="hero-content">
-        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <h1>YOUR FEET 
+            DESERVE 
+            THE BEST</h1>
         <p>
+
           YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
           SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
           SHOES.
         </p>
         <div className="hero-btn">
             <button>Shop Now</button>
-            <button>Category</button>
+            <button className="secondary-btn">Category</button>
         </div>
         <div className="shopping">
             <p>Also Available On</p>
             <div className="brand-icon">
             <img src="/images/flipkart.png" alt="flipkart" />
-            <img src="/images/amazon.png" alt="amazon" />
+            <img  src="/images/amazon.png" alt="amazon" />
             </div>
         </div>
       </div>
