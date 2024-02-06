077d0f0c98f7119fc61cd99e174743ce0e820b96
chandiosamad07 <absamadchandio@gmail.com>
2024-02-06 15:52:19 +0500

    contact page logo added

---
+++ contactpage/src/components/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const Nav = () => {
+  return (
+    <div>
+      <img className='h-72 w-72 translate-x-91 translate-y-2' src="/images/logo.png" alt="logo" />
+    </div>
+  )
+}
+
+export default Nav
