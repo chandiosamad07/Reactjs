decdc90723fbf916756c0924f6525b73d09c6afc
chandiosamad07 <absamadchandio@gmail.com>
2024-02-06 23:09:29 +0500

    Named export and import

--- contactpage/src/components/Nav.jsx
+++ contactpage/src/components/Nav.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 const Nav = () => {
   return (
-    <div>
-      <img className='h-72 w-72 translate-x-91 translate-y-2' src="/images/logo.png" alt="logo" />
+    <div className=' h-72 w-72'>
+      <img className='translate-x-91 translate-y-2' src="/images/logo.png" alt="logo" />
     </div>
   )
 }
--- onlinelearning/src/App.js
+++ onlinelearning/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Component from './components/component';
 import Profile from './components/Profile';
 import Gallery from './components/Gallery';
-
+import { Import } from './components/Import';
 function App() {
   return (
   <>
   {/* <Component />
   <Profile /> */}
   <Gallery />
+  <Import />
   </>
   );
 }
