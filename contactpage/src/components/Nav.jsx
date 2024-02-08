7171eea2da338eaae820bb9a8d9f34697f531ba8
chandiosamad07 <absamadchandio@gmail.com>
2024-02-08 21:18:30 +0500

    Contact us page Nav

--- contactpage/src/components/Nav.jsx
+++ contactpage/src/components/Nav.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 const Nav = () => {
   return (
+    <nav>
     <div className=' h-72 w-72'>
       <img className='translate-x-91 translate-y-2' src="/images/logo.png" alt="logo" />
     </div>
+    <ul>
+      <li>Home</li>
+      <li>ABOUT</li>
+      <li>CONTACT</li>
+    </ul>
+    </nav>
   )
 }
 
