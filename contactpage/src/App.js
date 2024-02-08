6b5f720a68e8af04f087938d5ea9600479b0c4ba
chandiosamad07 <absamadchandio@gmail.com>
2024-02-08 21:50:06 +0500

    Contact us page Header using tailwind css

--- contactpage/src/App.js
+++ contactpage/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Contactheader from './components/Contactheader';
 import Nav from './components/Nav';
 
 function App() {
   return (
   <>
   <Nav />
+  <Contactheader />
   </>
   );
 }
