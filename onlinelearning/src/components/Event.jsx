56cb92f45ed9af97ebc3e1839da25803b0a99601
chandiosamad07 <absamadchandio@gmail.com>
2024-02-13 19:16:34 +0500

    Event

--- onlinelearning/src/components/Event.jsx
+++ onlinelearning/src/components/Event.jsx
@@ -9,7 +9,7 @@ const Event = () => {
     <div>
       {/* <button onClick={handleClick}>Click Me</button> */}
 
-      <button className='border solide m-4' onClick={()=>{alert("Hello alert")}}>Click me</button>
+      <button className='border solide m-4' onClick={()=>{alert("Hello Samad")}}>Click me</button>
     </div>
   )
 }
