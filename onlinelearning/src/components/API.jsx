82f723e71dc2a25a0af4e7162eeae901623ef84e
chandiosamad07 <absamadchandio@gmail.com>
2024-02-19 12:43:41 +0500

    Another method to consume API data

--- onlinelearning/src/components/API.jsx
+++ onlinelearning/src/components/API.jsx
@@ -3,12 +3,33 @@ import React, {useState , useEffect} from 'react'
 
 const API = () => {
     const [post , setPosts] = useState([]);
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
-        .then(response => response.json())
-        .then(data => setPosts(data))
-        .catch(error => console.error('Error fetching posts: ', error));
+    const [data , setData] = useState([]);
+    //first method
 
+    // useEffect(()=>{
+    //     fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+    //     .then(response => response.json())
+    //     .then(data => setPosts(data))
+    //     .catch(error => console.error('Error fetching posts: ', error));
+
+    // },[])
+
+    //second method to fetch API
+    useEffect(()=>{
+        const fetchData = async () =>{
+       try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        if(!response.ok){
+            throw new Error('Failed to fetch data');
+        }
+        const josnData = await response.json();
+        setPosts(josnData);
+       }
+       catch{
+        console.log('Error Fetching Data: ')
+       }
+    }
+    fetchData();
     },[])
   return (
     <div>
