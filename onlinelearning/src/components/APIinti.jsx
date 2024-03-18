514f34e647d47841027e449122768f13f80b8e49
chandiosamad07 <absamadchandio@gmail.com>
2024-03-18 12:41:31 +0500

    Api Intigration with using fetch with try and catch

--- onlinelearning/src/components/APIinti.jsx
+++ onlinelearning/src/components/APIinti.jsx
@@ -4,12 +4,34 @@ import React from "react";
 const APIinti = () => {
   const [post, setPost] = useState([]);
 
+//first Method to API integrate
+// useEffect(()=>{
+// fetch('https://jsonplaceholder.typicode.com/posts')
+// .then(response => response.json())
+// .then(data => setPost(data))
+// .catch(error => console.error("error fetching" , error));
+// },[])
+
 useEffect(()=>{
-fetch('https://jsonplaceholder.typicode.com/posts')
-.then(response => response.json())
-.then(data => setPost(data))
-.catch(error => console.error("error fetching" , error));
+
+  const fetchApidata = async()=>{
+    try{
+        const respone = await fetch ("https://jsonplaceholder.typicode.com/posts")
+        if(!respone.ok){
+            throw new Error('Failed to fetch data');
+        }
+        const JsonData = await respone.json()
+        setPost(JsonData)
+        console.log(JsonData , "This json data")
+    }
+    catch{
+        console.log("Geting this error from API")
+    }
+  }
+  fetchApidata();
+
 },[])
+
   return (
     <div className="m-4 p-4">
      <h1>Post</h1>
