bd17a4ab4ea90f74b49c8c38c0a5a0aee8bf4153
chandiosamad07 <absamadchandio@gmail.com>
2024-03-19 12:09:02 +0500

    Call data to render body according side bar event

--- onlinelearning/src/components/Datapassing/Sidebar.jsx
+++ onlinelearning/src/components/Datapassing/Sidebar.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Sidebar = ({onSidebarClick}) => {
   return (
     <>
-    <button onClick={(onSidebarClick('option 1'))}>Option 1</button>
-    <button onClick={(onSidebarClick('option 2'))}>Option 2</button>
+    <button onClick={(onSidebarClick('option1'))}>Option 1</button>
+    <button onClick={(onSidebarClick('option2'))}>Option 2</button>
 
     </>
     )
