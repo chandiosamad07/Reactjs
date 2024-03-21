23328a59e50903b1e915251ad2e5733c1a6e73f3
chandiosamad07 <absamadchandio@gmail.com>
2024-03-21 20:51:46 +0500

    Hooks

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -25,31 +25,7 @@ function App() {
   return (
     <div className="App">
       <Game />
-      {/* <div>
-        <h1>Welcome to my App</h1>
-        <Mybutton />
-        <Mybutton />
-        <AboutPage />
-        <Profile/>
-      </div> */}
-      {/* <LifecycleA /> */}
-      {/* <Form></Form> */}
-      {/* <Inline /> */}
-     {/* <Stylesheet></Stylesheet> */}
-      {/* <NameList/> */}
-      {/* <UserGreeting /> */}
-      {/* <ParentComponent /> */}
-      {/* <EventBind /> */}
-      {/* <FuncationClick/>
-      <ClassClick/> */}
-      {/* <Hookcounter/> */}
-      {/* <Counter/> */}
-      {/* <Message/> */}   
-      {/* <Greet name="Abdul"/>
-      <Greet name="Samad"/>
-      <Greet name="Chandio"/>
-      <Welcome />
-      <Hello></Hello> */}
+      
     </div>
   );
 }
