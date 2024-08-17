9acc82d0772d40ba51454ffa81304368a59ead93
chandiosamad07 <absamadchandio@gmail.com>
2024-08-17 21:45:28 +0500

    useState Hook with prevCount

--- onlinelearning/src/components/Hooks1/Counter.jsx
+++ onlinelearning/src/components/Hooks1/Counter.jsx
@@ -2,10 +2,18 @@ import React, { useState } from 'react'
 
 const Counter = () => {
     const [count ,setCount] = useState(0)
+
+const   FiveTime = ()=>{
+        for(let i =0;i<5;i++){
+            setCount(preCount => preCount+1)
+        }
+    }
     
   return (
     <div>
       <button onClick={()=>setCount(count+1)}>Click me  {count}</button>
+      <br />
+      <button onClick={FiveTime}>Inc 5 time  {count}</button>
     </div>
   )
 }
